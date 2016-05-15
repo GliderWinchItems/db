1b5d7f1fc77d48ced6f2c609d7077f1395c9932f
Donald <dhaselwood@verizon.net>
2016-05-15 17:56:19 -0400

    Test

--- pcc_values_table/src/pcc_values_table/Pcc_values_table.java
+++ pcc_values_table/src/pcc_values_table/Pcc_values_table.java
@@ -97,6 +97,7 @@ public class Pcc_values_table {
             Integer count1 = 0;            
             String tmp = "";
             String old = "";
+String tmpsave = "";
             rs = stmt.executeQuery(query);
             while (rs.next()) {               
                 tmp = rs.getString("FUNCTION_NAME");
@@ -115,11 +116,11 @@ public class Pcc_values_table {
 
             System.out.format("#define %s_PARAM_SIZE %d\n\n",old,count1);
             
-            System.out.format("\n// ======================= SELECTION DEFINES ==========================\n"); 
-            System.out.format("/* The following #include is located in the directory with the app program and"
-       + "\n      selects the arrays for that particular app from the following which includes all.\n ");
-System.out.format("#include \"select_function.h\"\t// A #define selects from the following two array/table groups.\n"
-       + "//\t\t\t\t   1) parameters, and 2) unit/command canid\n\n");         
+//            System.out.format("\n// ======================= SELECTION DEFINES ==========================\n"); 
+//            System.out.format("/* The following #include is located in the directory with the app program and"
+//       + "\n      selects the arrays for that particular app from the following which includes all.*/\n ");
+//System.out.format("// #include \"select_function.h\"\t// A #define selects from the following two array/table groups.\n"
+//       + "//\t\t\t\t   1) parameters, and 2) unit/command canid\n\n");         
 // ========= Make param table ===============================            
             flag = 0; 
             count1 = 0; count = 0;  
@@ -127,19 +128,20 @@ System.out.format("#include \"select_function.h\"\t// A #define selects from the
             rs = stmt.executeQuery(query);
             while (rs.next()) {               
                 tmp = rs.getString("FUNCTION_NAME");
+tmpsave = tmp;                
                 /* Skip 'size' on first if new function name. */
                 if (!(tmp .equals(old))){
                     if (flag == 0){
                         flag = 1;
                     }else{
 //                        System.out.format("//  %s SIZE %d\n",old,count1);                        
-                        System.out.format("#endif\n"); // End for this function_name
+                        System.out.format("};\n#endif\n"); // End for this function_name
                         count = 0;
                      }
                     System.out.format("\n// =====================================================================\n");
                     System.out.format("#ifdef %s\t// Include following parameters?\n",tmp);
-                    System.out.format("uint32_t paramval[] = \n");
-                    System.out.format(" %s_PARAM_SIZE\t/* Number of param entries that follow */\n", tmp);
+                    System.out.format("const uint32_t paramval[] = {\n");
+                    System.out.format(" %s_PARAM_SIZE,\t/* Number of param entries that follow */\n", tmp);
                     count1 = 0;                    
 
                     old = tmp;
@@ -175,11 +177,30 @@ System.out.format("#include \"select_function.h\"\t// A #define selects from the
                 System.out.format(" %2d ",code);
                 System.out.format(" %-80s*/\n",rs.getString("DESCRIPTION"));
             }
-            System.out.format("#endif\n");
+            System.out.format("};\n#endif\n");
 // =========================================================================            
 // ========== Add command CAN IDs for functions ============================ 
 // =========================================================================     
-            String query3 = "SELECT * FROM FUNCTIONS ";
+  //          String query3 = "SELECT * FROM FUNCTIONS ";
+            
+ //           String query3 = "SELECT "
+//        + "FUNCTIONS.*,"
+//        + "FUNCTION_CODES.* \n"
+//        + "FROM FUNCTIONS \n"
+//            + "JOIN FUNCTION_CODES \n"
+//                + "ON FUNCTION_CODES.FUNCTION_NAME = FUNCTIONS.FUNCTION_NAME\n"
+//        + "ORDER BY FUNCTIONS.CAN_UNIT_NAME";
+            
+        String query3 = "SELECT FUNCTIONS.*,FUNCTION_CODES.*,CANID.CANID_HEX \n"
+            + "FROM FUNCTIONS \n"
+            + "JOIN FUNCTION_CODES \n"
+                +  "ON FUNCTION_CODES.FUNCTION_NAME = FUNCTIONS.FUNCTION_NAME\n"
+                    + "JOIN CAN_UNIT \n"
+                        + "ON CAN_UNIT.CAN_UNIT_NAME = FUNCTIONS.CAN_UNIT_NAME\n"
+                            + "JOIN CANID \n"
+                                + "ON CANID.CANID_NAME = CAN_UNIT.CANID_NAME\n"
+        + "ORDER BY FUNCTIONS.CAN_UNIT_NAME";
+          
             
                         rs = stmt.executeQuery(query);
             count = 0;
@@ -207,7 +228,7 @@ System.out.format("#include \"select_function.h\"\t// A #define selects from the
                 count1 += 1;    
             }
 
-            System.out.format("\n#define %sCMDID_TABLE_SIZE %d\n\n",old,count1);  
+            System.out.format("\n#define %s_CMDID_TABLE_SIZE %d\n\n",old,count1);  
         
     // ========= Make table command CANID =============================== 
     
@@ -222,13 +243,15 @@ System.out.format("#include \"select_function.h\"\t// A #define selects from the
                     if (flag == 0){
                         flag = 1;
                     }else{                        
-                        System.out.format("#endif\n"); // End for this unit
+                        System.out.format("};\n#endif\n"); // End for this unit
                         count = 0;
                      }
+                    String yid = "";    // Get unit CAN ID
+                    yid = rs.getString("CANID_HEX");
                     System.out.format("\n// =====================================================================\n");
                     System.out.format("#ifdef %s\t// a #define is used to select the following\n",tmp);
-                    System.out.format("uint32_t canid_cmd[] = \n");
-                    System.out.format("   %s_CMDID_TABLE_SIZE\t/* Number of CAN IDs that follow */\n", tmp);
+                    System.out.format("const struct FUNC_CANID func_canid[] = {\n");
+                    System.out.format("{   0x%s, %s_CMDID_TABLE_SIZE },\t/* {Unit CAN ID, Number of CAN IDs that follow} */\n", yid,tmp);
                     count = 0;                    
 
                     old = tmp;
@@ -238,12 +261,12 @@ System.out.format("#include \"select_function.h\"\t// A #define selects from the
                 String y = "";
                 y = getCANID(stmt2, val);
                 count += 1;
-                System.out.format("   0x%s , /* %3d ",y, count);
+                System.out.format("{%4d,  0x%s }, /* %3d ",rs.getInt("FUNCTION_CODE"),y, count);
                 System.out.format("%-18s ",rs.getString("CAN_UNIT_NAME"));
                 System.out.format("  %s\t", val);
                 System.out.format(" %-30s*/\n",rs.getString("DESCRIPTION"));
             }
-            System.out.format("#endif\n");        
+            System.out.format("};\n#endif\n");        
 
 // ========================================   
 
